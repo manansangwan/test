7b961c7d0ac22e37ac1ce8eed10622b7ae951c01
manansangwan <manansangwan007@gmail.com>
2020-12-07 16:30:56 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+platform = new Ground(90,90,150,170);
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
